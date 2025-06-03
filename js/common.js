87bdaeb13533e4217dd0be23f08b71cf96723ded
Viktoria Zaharova <zaharova181114@gmail.com>
2025-06-03 23:04:01 +0300

    basket

--- js/common.js
+++ js/common.js
@@ -208,3 +208,22 @@ $('.btn-burger').on('click', function (e) {
 Fancybox.bind("[data-fancybox]", {
   // Your custom options
 });
+
+// amount
+$('.down').on("click", function () {
+    let $input = $(this).parent().find('input');
+    let count = parseInt($input.val()) - 1;
+    count = count < 1 ? 1 : count;
+    $input.val(count);
+    $input.change();
+    return false;
+});
+$('.up').on("click",function () {
+    let $input = $(this).parent().find('input');
+    $input.val(parseInt($input.val()) + 1);
+    $input.change();
+    return false;
+});
+$('.amount input').on('input', function () {
+	$(this).val($(this).val().replace(/[A-Za-zА-Яа-яЁё]/, ''))
+});
